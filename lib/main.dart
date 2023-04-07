e17bc399ef4534f47fef5f002dc7548a832664b9
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-07 02:41:29 +0200

    init the application

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FivelyEcommerce());
+}
+
+class FivelyEcommerce extends StatelessWidget {
+  const FivelyEcommerce({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
